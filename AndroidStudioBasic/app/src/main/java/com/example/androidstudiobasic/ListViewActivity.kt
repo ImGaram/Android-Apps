94bccc9c8f638367e09e21d56e5d39afcf030a39
imgaram <s21049@gsm.hs.kr>
2021-10-06 23:06:21 +0900

    android recyclerview, listview

--- AndroidStudioBasic/app/src/main/java/com/example/androidstudiobasic/ListViewActivity.kt
+++ AndroidStudioBasic/app/src/main/java/com/example/androidstudiobasic/ListViewActivity.kt
@@ -2,11 +2,13 @@ package com.example.androidstudiobasic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_list_view.*
 
 class ListViewActivity : AppCompatActivity() {
@@ -15,12 +17,18 @@ class ListViewActivity : AppCompatActivity() {
         setContentView(R.layout.activity_list_view)
 
         val carList = ArrayList<CarForList>()
-        for (i in 0 until 10){
+        for (i in 0 until 50){
             carList.add(CarForList(""+i+"번째 차",""+i+"순위 엔진"))
         }
 
         val adapter = ListViewAdapter(carList, LayoutInflater.from(this@ListViewActivity))
         listview.adapter = adapter
+        listview.setOnItemClickListener { parent, view, position, id ->
+            val carName = (adapter.getItem(position) as CarForList).name
+            val carEngine = (adapter.getItem(position) as CarForList).engine
+
+            Toast.makeText(this@ListViewActivity, carName + " " + carEngine, Toast.LENGTH_SHORT).show()
+        }
     }
 }
 
@@ -45,13 +53,33 @@ class ListViewAdapter(
     }
     // 리스트의 아이디 부여
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val view = layoutInflater.inflate(R.layout.item_view, null)
-        val carNameTextView = view.findViewById<TextView>(R.id.car_name)
-        val carEngineTextView = view.findViewById<TextView>(R.id.car_engine)
+        val view: View
+        val holder: ViewHolder
 
-        carNameTextView.setText(carForList.get(position).name)
-        carEngineTextView.setText(carForList.get(position).engine)
+        if (convertView == null){
+            Log.d("convert", "1")
+            view = layoutInflater.inflate(R.layout.item_view,null)
+            holder = ViewHolder()
+            // findViewById 로 찾은 다음 holder 에 집어넣음
+            holder.carName = view.findViewById(R.id.car_name)
+            holder.carEngine = view.findViewById(R.id.car_engine)
+            // holder 를 찾게 함
+            view.tag = holder
+        }
+        else {
+            Log.d("convert", "2")
+            holder = convertView.tag as ViewHolder
+            view = convertView
+        }
+        holder.carName?.setText(carForList.get(position).name)
+        holder.carEngine?.setText(carForList.get(position).engine)
+
         return view
     }
 
+}
+
+class ViewHolder{
+    var carName: TextView? = null
+    var carEngine: TextView? = null
 }
