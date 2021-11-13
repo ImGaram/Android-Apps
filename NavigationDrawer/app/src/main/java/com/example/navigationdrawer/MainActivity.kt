13017dc5d11db07b5ee29a5f7f74e12e85635bf4
imgaram <s21049@gsm.hs.kr>
2021-11-13 14:00:33 +0900

    Navigation Drawer 연습

---
+++ NavigationDrawer/app/src/main/java/com/example/navigationdrawer/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.navigationdrawer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.core.view.GravityCompat
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        ham_img.setOnClickListener {
+            main_drawer_layout.openDrawer(GravityCompat.START)    // 네비게이션 드로어 열기
+        }
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home->{ // 메뉴 버튼
+                main_drawer_layout.openDrawer(GravityCompat.START)    // 네비게이션 드로어 열기
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
